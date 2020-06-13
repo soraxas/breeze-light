562260a45073cb67f15e8f6f198712bef3134efa
Tin Lai <oscar@tinyiu.com>
2020-06-13 17:08:06 +1000

    finish main implementation

--- breeze-light.fish
+++ breeze-light.fish
@@ -75,3 +75,103 @@ function breeze
 end
 
 breeze
+
+
+
+function parse_user_input
+
+    set -l file_names (git status --porcelain | string sub --start 4)
+
+    set num_files (count $file_names)
+
+
+    function __breeze_sanity_chk_start_num
+        if not test $argv[1] -gt 0
+            echo "[ERROR]: starting num '$argv[1]' must be > 0. Skipping."
+            return 1
+        end
+        return 0
+    end
+
+    function __breeze_sanity_chk_end_num
+        set -l num_files $argv[2]
+        if not test $argv[1] -le $num_files 
+            echo "[ERROR]: ending num '$argv[1]' must be <= range '$num_files'. Skipping."
+            return 1
+        end
+        return 0
+    end
+
+    function __breeze_sanity_chk_start_end_num
+        # ensure n is smaller than m
+        if not test $argv[1] -lt $argv[2]
+            echo "[ERROR]: starting num '$argv[1]' must be < ending num '$argv[2]'. Skipping."
+            return 1
+        end
+        return 0
+    end
+
+    function __breeze_echo_range_files
+        set -l start_n $argv[1]
+        set -l end_n $argv[2]
+        set -e argv[1]
+        set -e argv[1]
+        for i in (seq $start_n $end_n)
+            echo $argv[$i]
+        end
+    end
+    # set -l num_files $argv[1]
+
+    set -l target_files
+
+    for arg in $argv
+
+        # for normal case of one single numeric
+        if string match -q -r -- '^[0-9]+$' $arg
+            set -a target_files (echo $file_names[$arg])
+
+        # for case n-m
+        else if string match -q -r -- '^[0-9]+-[0-9]+$' $arg
+            set -l idxes (string split '-' $arg)
+            __breeze_sanity_chk_start_end_num $idxes[1] $idxes[2]
+            or continue
+            __breeze_sanity_chk_start_num $idxes[1]
+            or continue
+            __breeze_sanity_chk_end_num $idxes[2] $num_files
+            or continue
+
+            set -a target_files (__breeze_echo_range_files $idxes[1] $idxes[2] $file_names)
+
+        # for case n- (implied end)
+        else if string match -q -r -- '^[0-9]+-$' $arg
+            # ensure n is > 0
+            set arg (string sub --length (math (string length -- $arg)"-1") -- $arg)
+
+            __breeze_sanity_chk_start_num $arg
+            or continue
+            __breeze_sanity_chk_end_num $arg $num_files
+            or continue
+          
+            set -a target_files (__breeze_echo_range_files $arg $num_files $file_names)
+
+        # for case -m (implied start)
+        else if string match -q -r -- '^-[0-9]+$' $arg
+            # ensure m is < num_files
+            set arg (string sub --start 2 -- $arg)
+
+            __breeze_sanity_chk_start_num $arg
+            or continue
+            __breeze_sanity_chk_end_num $arg $num_files
+            or continue
+          
+            set -a target_files (__breeze_echo_range_files 1 $arg $file_names)
+
+        # probably is file name
+        else
+            set -a target_files $arg
+        end
+
+        # echo $arg
+    end
+    echo $target_files
+end
