bf66fe74e7528fd704c0360cbb41e84fa6eff6d2
Tin Lai <tin@tinyiu.com>
2024-07-26 13:10:24 +1000

    fix completion
    Signed-off-by: Tin Lai <tin@tinyiu.com>

--- completions/breeze.fish
+++ completions/breeze.fish
@@ -1,6 +1,6 @@
 
 complete -c breeze -n '__fish_use_subcommand' -xa "(breeze _complete)"
 
-complete -c breeze -n '__fish_seen_subcommand_from add' -l dry -d 'Dry-run by only echo-ing what will be performed'
+complete -c breeze -l dry -d 'Dry-run by only echo-ing what will be performed'
 complete -c breeze -n '__fish_seen_subcommand_from add' -s s -l show-status -d 'Display status after git add'
 complete -c breeze -n '__fish_seen_subcommand_from add' -s m -d 'Perform commit, and everything after this flag will the message'
